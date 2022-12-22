c898f6dd9bf163f041a8d263fe3adf881df4a199
Gonzalo Pascual <gonzalopascual.gpc@gmail.com>
2022-12-22 13:01:00 +0100

    refactor: iterates items through a foreach

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -7,54 +7,52 @@ class GildedRose {
 	public static function updateQuality(
 		$items
 	) {
-		for ($i = 0; $i < count($items); $i++) {
-			if (("Aged Brie" != $items[$i]->getName()) && ("Backstage passes to a TAFKAL80ETC concert" != $items[$i]->getName())) {
-				if ($items[$i]->getQuality() > 0) {
-					if ("Sulfuras, Hand of Ragnaros" != $items[$i]->getName()) {
-						$items[$i]->setQuality($items[$i]->getQuality() - 1);
+		foreach ($items as $iValue) {
+			if (("Aged Brie" != $iValue->getName()) && ("Backstage passes to a TAFKAL80ETC concert" != $iValue->getName())) {
+				if ($iValue->getQuality() > 0) {
+					if ("Sulfuras, Hand of Ragnaros" != $iValue->getName()) {
+						$iValue->setQuality($iValue->getQuality() - 1);
 					}
 				}
 			} else {
-				if ($items[$i]->getQuality() < 50) {
-					$items[$i]->setQuality($items[$i]->getQuality() + 1);
-					if ("Backstage passes to a TAFKAL80ETC concert" == $items[$i]->getName()) {
-						if ($items[$i]->getSellIn() < 11) {
-							if ($items[$i]->getQuality() < 50) {
-								$items[$i]->setQuality($items[$i]->getQuality() + 1);
+				if ($iValue->getQuality() < 50) {
+					$iValue->setQuality($iValue->getQuality() + 1);
+					if ("Backstage passes to a TAFKAL80ETC concert" == $iValue->getName()) {
+						if ($iValue->getSellIn() < 11) {
+							if ($iValue->getQuality() < 50) {
+								$iValue->setQuality($iValue->getQuality() + 1);
 							}
 						}
-						if ($items[$i]->getSellIn() < 6) {
-							if ($items[$i]->getQuality() < 50) {
-								$items[$i]->setQuality($items[$i]->getQuality() + 1);
+						if ($iValue->getSellIn() < 6) {
+							if ($iValue->getQuality() < 50) {
+								$iValue->setQuality($iValue->getQuality() + 1);
 							}
 						}
 					}
 				}
 			}
 
-			if ("Sulfuras, Hand of Ragnaros" != $items[$i]->getName()) {
-				$items[$i]->setSellIn($items[$i]->getSellIn() - 1);
+			if ("Sulfuras, Hand of Ragnaros" != $iValue->getName()) {
+				$iValue->setSellIn($iValue->getSellIn() - 1);
 			}
 
-			if ($items[$i]->getSellIn() < 0) {
-				if ("Aged Brie" != $items[$i]->getName()) {
-					if ("Backstage passes to a TAFKAL80ETC concert" != $items[$i]->getName()) {
-						if ($items[$i]->getQuality() > 0) {
-							if ("Sulfuras, Hand of Ragnaros" != $items[$i]->getName()) {
-								$items[$i]->setQuality($items[$i]->getQuality() - 1);
+			if ($iValue->getSellIn() < 0) {
+				if ("Aged Brie" != $iValue->getName()) {
+					if ("Backstage passes to a TAFKAL80ETC concert" != $iValue->getName()) {
+						if ($iValue->getQuality() > 0) {
+							if ("Sulfuras, Hand of Ragnaros" != $iValue->getName()) {
+								$iValue->setQuality($iValue->getQuality() - 1);
 							}
 						}
 					} else {
-						$items[$i]->setQuality($items[$i]->getQuality() - $items[$i]->getQuality());
+						$iValue->setQuality($iValue->getQuality() - $iValue->getQuality());
 					}
 				} else {
-					if ($items[$i]->getQuality() < 50) {
-						$items[$i]->setQuality($items[$i]->getQuality() + 1);
+					if ($iValue->getQuality() < 50) {
+						$iValue->setQuality($iValue->getQuality() + 1);
 					}
 				}
 			}
 		}
 	}
 }
-
-?>
