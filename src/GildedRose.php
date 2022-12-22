5b24d40e7d300542a690c3852c6df80f13370271
Gonzalo Pascual <gonzalopascual.gpc@gmail.com>
2022-12-22 13:28:09 +0100

    refactor: simplifies code related to "Aged Brie"

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -2,21 +2,40 @@
 
 require_once 'src/Item.php';
 
-class GildedRose {
+class GildedRose
+{
 
-	public static function updateQuality(
-		$items
-	) {
-		foreach ($items as $iValue) {
-			if ("Aged Brie" == $iValue->getName()) {
+
+    public static function updateQuality(
+        $items
+    ) {
+        foreach ($items as $iValue) {
+            if ('Aged Brie' == $iValue->getName()) {
                 if ($iValue->getQuality() < 50) {
                     $iValue->setQuality($iValue->getQuality() + 1);
-                    if ("Backstage passes to a TAFKAL80ETC concert" == $iValue->getName()) {
+                }
+
+                $iValue->setSellIn($iValue->getSellIn() - 1);
+
+                if ($iValue->getSellIn() < 0) {
+                    if ($iValue->getQuality() < 50) {
+                        $iValue->setQuality($iValue->getQuality() + 1);
+                    }
+                }
+
+                return;
+            }
+
+            if ('Backstage passes to a TAFKAL80ETC concert' == $iValue->getName()) {
+                if ($iValue->getQuality() < 50) {
+                    $iValue->setQuality($iValue->getQuality() + 1);
+                    if ('Backstage passes to a TAFKAL80ETC concert' == $iValue->getName()) {
                         if ($iValue->getSellIn() < 11) {
                             if ($iValue->getQuality() < 50) {
                                 $iValue->setQuality($iValue->getQuality() + 1);
                             }
                         }
+
                         if ($iValue->getSellIn() < 6) {
                             if ($iValue->getQuality() < 50) {
                                 $iValue->setQuality($iValue->getQuality() + 1);
@@ -24,76 +43,32 @@ class GildedRose {
                         }
                     }
                 }
-
-                if ("Sulfuras, Hand of Ragnaros" != $iValue->getName()) {
-                    $iValue->setSellIn($iValue->getSellIn() - 1);
-                }
-
-                if ($iValue->getSellIn() < 0) {
-                    if ("Aged Brie" == $iValue->getName()) {
-                        if ($iValue->getQuality() < 50) {
-                            $iValue->setQuality($iValue->getQuality() + 1);
-                        }
-                    } else {
-                        if ("Backstage passes to a TAFKAL80ETC concert" != $iValue->getName()) {
-                            if ($iValue->getQuality() > 0) {
-                                if ("Sulfuras, Hand of Ragnaros" != $iValue->getName()) {
-                                    $iValue->setQuality($iValue->getQuality() - 1);
-                                }
-                            }
-                        } else {
-                            $iValue->setQuality($iValue->getQuality() - $iValue->getQuality());
-                        }
-                    }
-                    return;
-                }
             } else {
-                if ("Backstage passes to a TAFKAL80ETC concert" == $iValue->getName()) {
-                    if ($iValue->getQuality() < 50) {
-                        $iValue->setQuality($iValue->getQuality() + 1);
-                        if ("Backstage passes to a TAFKAL80ETC concert" == $iValue->getName()) {
-                            if ($iValue->getSellIn() < 11) {
-                                if ($iValue->getQuality() < 50) {
-                                    $iValue->setQuality($iValue->getQuality() + 1);
-                                }
-                            }
-                            if ($iValue->getSellIn() < 6) {
-                                if ($iValue->getQuality() < 50) {
-                                    $iValue->setQuality($iValue->getQuality() + 1);
-                                }
-                            }
-                        }
+                if ($iValue->getQuality() > 0) {
+                    if ('Sulfuras, Hand of Ragnaros' != $iValue->getName()) {
+                        $iValue->setQuality($iValue->getQuality() - 1);
                     }
-                } else {
+                }
+            }//end if
+            // end if
+            if ('Sulfuras, Hand of Ragnaros' != $iValue->getName()) {
+                $iValue->setSellIn($iValue->getSellIn() - 1);
+            }
+
+            if ($iValue->getSellIn() < 0) {
+                if ('Backstage passes to a TAFKAL80ETC concert' != $iValue->getName()) {
                     if ($iValue->getQuality() > 0) {
-                        if ("Sulfuras, Hand of Ragnaros" != $iValue->getName()) {
+                        if ('Sulfuras, Hand of Ragnaros' != $iValue->getName()) {
                             $iValue->setQuality($iValue->getQuality() - 1);
                         }
                     }
+                } else {
+                    $iValue->setQuality($iValue->getQuality() - $iValue->getQuality());
                 }
             }
+        }//end foreach
 
-			if ("Sulfuras, Hand of Ragnaros" != $iValue->getName()) {
-				$iValue->setSellIn($iValue->getSellIn() - 1);
-			}
+    }//end updateQuality()
 
-			if ($iValue->getSellIn() < 0) {
-				if ("Aged Brie" == $iValue->getName()) {
-                    if ($iValue->getQuality() < 50) {
-                        $iValue->setQuality($iValue->getQuality() + 1);
-                    }
-                } else {
-                    if ("Backstage passes to a TAFKAL80ETC concert" != $iValue->getName()) {
-                        if ($iValue->getQuality() > 0) {
-                            if ("Sulfuras, Hand of Ragnaros" != $iValue->getName()) {
-                                $iValue->setQuality($iValue->getQuality() - 1);
-                            }
-                        }
-                    } else {
-                        $iValue->setQuality($iValue->getQuality() - $iValue->getQuality());
-                    }
-                }
-			}
-		}
-	}
-}
+
+}//end class
