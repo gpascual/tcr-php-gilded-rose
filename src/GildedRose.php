5515be99b95c11403b89c463a7ed3b8eda3fd6cd
Gonzalo Pascual <gonzalopascual.gpc@gmail.com>
2022-12-22 13:39:59 +0100

    refactor: simplifies code related to "Backstage"

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -29,43 +29,31 @@ class GildedRose
             if ('Backstage passes to a TAFKAL80ETC concert' == $iValue->getName()) {
                 if ($iValue->getQuality() < 50) {
                     $iValue->setQuality($iValue->getQuality() + 1);
-                    if ('Backstage passes to a TAFKAL80ETC concert' == $iValue->getName()) {
-                        if ($iValue->getSellIn() < 11) {
-                            if ($iValue->getQuality() < 50) {
-                                $iValue->setQuality($iValue->getQuality() + 1);
-                            }
+                    if ($iValue->getSellIn() < 11) {
+                        if ($iValue->getQuality() < 50) {
+                            $iValue->setQuality($iValue->getQuality() + 1);
                         }
+                    }
 
-                        if ($iValue->getSellIn() < 6) {
-                            if ($iValue->getQuality() < 50) {
-                                $iValue->setQuality($iValue->getQuality() + 1);
-                            }
+                    if ($iValue->getSellIn() < 6) {
+                        if ($iValue->getQuality() < 50) {
+                            $iValue->setQuality($iValue->getQuality() + 1);
                         }
                     }
                 }
 
-                if ('Sulfuras, Hand of Ragnaros' != $iValue->getName()) {
-                    $iValue->setSellIn($iValue->getSellIn() - 1);
-                }
+                $iValue->setSellIn($iValue->getSellIn() - 1);
 
                 if ($iValue->getSellIn() < 0) {
-                    if ('Backstage passes to a TAFKAL80ETC concert' != $iValue->getName()) {
-                        if ($iValue->getQuality() > 0) {
-                            if ('Sulfuras, Hand of Ragnaros' != $iValue->getName()) {
-                                $iValue->setQuality($iValue->getQuality() - 1);
-                            }
-                        }
-                    } else {
-                        $iValue->setQuality($iValue->getQuality() - $iValue->getQuality());
-                    }
+                    $iValue->setQuality($iValue->getQuality() - $iValue->getQuality());
                 }
 
                 return;
-            } else {
-                if ($iValue->getQuality() > 0) {
-                    if ('Sulfuras, Hand of Ragnaros' != $iValue->getName()) {
-                        $iValue->setQuality($iValue->getQuality() - 1);
-                    }
+            }//end if
+
+            if ($iValue->getQuality() > 0) {
+                if ('Sulfuras, Hand of Ragnaros' != $iValue->getName()) {
+                    $iValue->setQuality($iValue->getQuality() - 1);
                 }
             }//end if
             // end if
@@ -74,14 +62,10 @@ class GildedRose
             }
 
             if ($iValue->getSellIn() < 0) {
-                if ('Backstage passes to a TAFKAL80ETC concert' != $iValue->getName()) {
-                    if ($iValue->getQuality() > 0) {
-                        if ('Sulfuras, Hand of Ragnaros' != $iValue->getName()) {
-                            $iValue->setQuality($iValue->getQuality() - 1);
-                        }
+                if ($iValue->getQuality() > 0) {
+                    if ('Sulfuras, Hand of Ragnaros' != $iValue->getName()) {
+                        $iValue->setQuality($iValue->getQuality() - 1);
                     }
-                } else {
-                    $iValue->setQuality($iValue->getQuality() - $iValue->getQuality());
                 }
             }
         }//end foreach
